2204ab5f0682ff7ca88ccc11e04ff545495ca978
angelinamitri <lina.mitry96@gmail.com>
2021-08-18 13:28:12 +0300

    refactoring

--- src/main/java/com/text/compiler/App.java
+++ src/main/java/com/text/compiler/App.java
@@ -6,18 +6,20 @@ import com.text.compiler.validator.SimpleValidator;
 import com.text.compiler.writer.FileWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App {
     public static void main(String[] args) {
-        var pathInput = "file.txt";
-        var pathOutput = "output.txt";
+        var pathInput = Objects.requireNonNull(App.class.getClassLoader().getResource("file.txt")).getPath();
+        var pathOutput = "src/main/resources/output.txt";
 
         var formatter = new SimpleFormatter(new SimpleValidator());
 
-        try (var fr = new FileReader(new File(pathInput));
-             var fw = new FileWriter(new File(pathOutput))) {
+        try (var fr = new FileReader(new File(pathInput).getAbsoluteFile());
+             var fw = new FileWriter(new File(pathOutput).getAbsoluteFile())) {
             log.info(formatter.format(fr, fw));
         } catch (IOException e) {
             log.error("Format failed", e);
